163a148c03c1a934fc3c22249ac7f2b7dfe4b6c0
mtan1405 <132926494+mtan1405@users.noreply.github.com>
2023-05-14 11:36:11 +0700

    Update InforAboutTable.java

--- ProjectMusic/app/src/main/java/com/example/projectmusic/DBForMusic/InforAboutTable.java
+++ ProjectMusic/app/src/main/java/com/example/projectmusic/DBForMusic/InforAboutTable.java
@@ -1,7 +1,7 @@
 package com.example.projectmusic.DBForMusic;
 
 public class InforAboutTable {
-    public final static String   DATABASE_NAME  = "database_music12" ;
+    public final static String   DATABASE_NAME  = "database_music0" ;
     public static final String   TABLE1_NAME = "song12" ;
     public static final  String   NAME_COLUMN_OF_TABLE1 = "name" ;
     public static final String   IMAGE_COLUMN_OF_TABLE1 = "image" ;
